471c0720e6c6f4c58010b97a321ae39e8e700bef
Abed Jallad <MCC@DESKTOP-B357F9G>
2026-07-13 17:47:16 +0300

    create essantial file

---
+++ Data Access Layer/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Data_Access_Layer
+{
+    public class Class1
+    {
+    }
+}
---
+++ Data Access Layer/clsDataAccessSetting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace Data_Access_Layer
+{
+    internal class clsDataAccessSetting
+    {
+
+       public static string connectionString = "server=.;database=DVLD;user id=sa;password=123456";
+
+    }
+}
