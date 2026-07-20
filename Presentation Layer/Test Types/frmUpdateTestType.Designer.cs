5217b0b5c01acd36268312e3c19647b117f2277f
Abed Jallad <MCC@DESKTOP-B357F9G>
2026-07-21 01:33:04 +0300

    Add frmUpdateTestType

---
+++ Presentation Layer/Test Types/frmUpdateTestType.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace DVLD.Presentation_Layer
+{
+    partial class frmUpdateTestType
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.gbTestTypeInformation = new System.Windows.Forms.GroupBox();
+            this.lblIDValue = new System.Windows.Forms.Label();
+            this.lblIDTitle = new System.Windows.Forms.Label();
+            this.txtTestTitle = new System.Windows.Forms.TextBox();
+            this.lblTestTitle = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.txtFees = new System.Windows.Forms.TextBox();
+            this.lblFees = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.pnlHeader.SuspendLayout();
+            this.gbTestTypeInformation.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+            this.pnlHeader.Controls.Add(this.lblTitle);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(650, 75);
+            this.pnlHeader.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(20, 16);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(269, 41);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Update Test Type";
+            // 
+            // gbTestTypeInformation
+            // 
+            this.gbTestTypeInformation.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbTestTypeInformation.BackColor = System.Drawing.Color.White;
+            this.gbTestTypeInformation.Controls.Add(this.lblIDValue);
+            this.gbTestTypeInformation.Controls.Add(this.lblIDTitle);
+            this.gbTestTypeInformation.Controls.Add(this.txtTestTitle);
+            this.gbTestTypeInformation.Controls.Add(this.lblTestTitle);
+            this.gbTestTypeInformation.Controls.Add(this.txtDescription);
+            this.gbTestTypeInformation.Controls.Add(this.lblDescription);
+            this.gbTestTypeInformation.Controls.Add(this.txtFees);
+            this.gbTestTypeInformation.Controls.Add(this.lblFees);
+            this.gbTestTypeInformation.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbTestTypeInformation.Location = new System.Drawing.Point(25, 95);
+            this.gbTestTypeInformation.Name = "gbTestTypeInformation";
+            this.gbTestTypeInformation.Size = new System.Drawing.Size(600, 320);
+            this.gbTestTypeInformation.TabIndex = 1;
+            this.gbTestTypeInformation.TabStop = false;
+            this.gbTestTypeInformation.Text = "Test Type Information";
+            // 
+            // lblIDTitle
+            // 
+            this.lblIDTitle.AutoSize = true;
+            this.lblIDTitle.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIDTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.lblIDTitle.Location = new System.Drawing.Point(30, 40);
+            this.lblIDTitle.Name = "lblIDTitle";
+            this.lblIDTitle.Size = new System.Drawing.Size(27, 19);
+            this.lblIDTitle.TabIndex = 0;
+            this.lblIDTitle.Text = "ID:";
+            // 
+            // lblIDValue
+            // 
+            this.lblIDValue.AutoSize = true;
+            this.lblIDValue.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIDValue.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.lblIDValue.Location = new System.Drawing.Point(120, 40);
+            this.lblIDValue.Name = "lblIDValue";
+            this.lblIDValue.Size = new System.Drawing.Size(42, 19);
+            this.lblIDValue.TabIndex = 1;
+            this.lblIDValue.Text = "[???]";
+            // 
+            // lblTestTitle
+            // 
+            this.lblTestTitle.AutoSize = true;
+            this.lblTestTitle.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTestTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.lblTestTitle.Location = new System.Drawing.Point(30, 80);
+            this.lblTestTitle.Name = "lblTestTitle";
+            this.lblTestTitle.Size = new System.Drawing.Size(42, 19);
+            this.lblTestTitle.TabIndex = 2;
+            this.lblTestTitle.Text = "Title:";
+            // 
+            // txtTestTitle
+            // 
+            this.txtTestTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTestTitle.Location = new System.Drawing.Point(120, 77);
+            this.txtTestTitle.Name = "txtTestTitle";
+            this.txtTestTitle.Size = new System.Drawing.Size(440, 25);
+            this.txtTestTitle.TabIndex = 3;
+            this.txtTestTitle.TextChanged += new System.EventHandler(this.txtTestTitle_TextChanged);
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescription.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.lblDescription.Location = new System.Drawing.Point(30, 120);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(89, 19);
+            this.lblDescription.TabIndex = 4;
+            this.lblDescription.Text = "Description:";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDescription.Location = new System.Drawing.Point(120, 117);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDescription.Size = new System.Drawing.Size(440, 130);
+            this.txtDescription.TabIndex = 5;
+            this.txtDescription.TextChanged += new System.EventHandler(this.txtDescription_TextChanged);
+            // 
+            // lblFees
+            // 
+            this.lblFees.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblFees.AutoSize = true;
+            this.lblFees.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFees.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.lblFees.Location = new System.Drawing.Point(30, 270);
+            this.lblFees.Name = "lblFees";
+            this.lblFees.Size = new System.Drawing.Size(43, 19);
+            this.lblFees.TabIndex = 6;
+            this.lblFees.Text = "Fees:";
+            // 
+            // txtFees
+            // 
+            this.txtFees.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFees.Location = new System.Drawing.Point(120, 267);
+            this.txtFees.Name = "txtFees";
+            this.txtFees.Size = new System.Drawing.Size(440, 25);
+            this.txtFees.TabIndex = 7;
+            this.txtFees.TextChanged += new System.EventHandler(this.txtFees_TextChanged);
+            this.txtFees.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFees_KeyPress);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(525, 435);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 40);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(410, 435);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(100, 40);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmUpdateTestType
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(247)))), ((int)(((byte)(250)))));
+            this.ClientSize = new System.Drawing.Size(650, 500);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.gbTestTypeInformation);
+            this.Controls.Add(this.pnlHeader);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinimumSize = new System.Drawing.Size(600, 500);
+            this.Name = "frmUpdateTestType";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Update Test Type";
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            this.gbTestTypeInformation.ResumeLayout(false);
+            this.gbTestTypeInformation.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.GroupBox gbTestTypeInformation;
+        private System.Windows.Forms.Label lblIDTitle;
+        private System.Windows.Forms.Label lblIDValue;
+        private System.Windows.Forms.Label lblTestTitle;
+        private System.Windows.Forms.TextBox txtTestTitle;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label lblFees;
+        private System.Windows.Forms.TextBox txtFees;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ Presentation Layer/Test Types/frmUpdateTestType.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DVLD.Presentation_Layer
+{
+    public partial class frmUpdateTestType : Form
+    {
+        public frmUpdateTestType()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTestTitle_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDescription_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFees_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void txtFees_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
